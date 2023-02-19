92a4f794a0f81df773a9ee6e029c1e9c6846acf9
신준혁 <oridori2705@naver.com>
2023-02-19 20:35:00 +0900

    주석추가

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENV=="production"){
-    module.exports= require('./prod');
+if(process.env.NODE_ENV=="production"){ //process.env.NODE_ENV 는 로컬환경이면 "development", 배포한 후면 "production"을 반환한다.
+    module.exports= require('./prod'); //배포 후 환경이면 prod 파일로 가서 mongoURI를 가져온다
 }else{
-    module.exports=require("./dev");
+    module.exports=require("./dev"); //로컬환경이면 .gitignore한 파일인 dev.js를 가져온다. 이로써 비밀설정이 된다.
 }
