d04b870532dd352b5a2cf97e475b1d83d32ec6c1
신준혁 <oridori2705@naver.com>
2023-02-19 21:27:24 +0900

    Bcrypt로 비밀번호 암호화기능 추가
    Bcrypt로 비밀번호 암호화기능 추가 및 Bcrypt 설치

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose=require('mongoose');
+
+const bcrypt =require("bcrypt");//비밀번호 암호화 설정 : bcrypt를 가져온다.
+const saltRounds=10;
+
 //스키마 생성
 const userSchema = mongoose.Schema({
     name : { //이름 : 타입은 string , 크기는 50
@@ -29,6 +33,29 @@ const userSchema = mongoose.Schema({
     tokenExp : { //토큰을 사용할 수 있는 유효기간
         type : Number
     }
+
+})
+//비밀번호 암호화 : 몽구스에서 가져온 메소드 pre("save") -> 유저정보를 저장하기 전에 무엇을 한다는 의미, next파라미터를 줘서 안의 함수가 끝나면 save로 넘어간다.
+userSchema.pre("save",function(next){
+
+    var user=this;//위의 userSchema를 가르킬수 있게 된다. 아래 hash에서 user.password를 하면 패스워드를 가져올 수 있다.
+    //만약에 사용자가 비밀번호를 변경할 때가 아닌 이메일을 변경할 때,이름을 변경할 때 등 여러가지 상황이 있는데 이 상황에 변경할 때 새로 저장하는데 다시 비밀번호를 암호화하면 안된다.
+    //그래서 비밀번호가 변경될 때만 비밀번호를 암호화해야한다.
+    if(user.isModified("password")){ //비밀번호가 변경될때의 조건
+       
+        //salt를 이용해서 비밀번호를 암호화 -> 먼저 salt를 생성 ->saltRounds가 필요 -> 이게 10이면 10자리인 salt를 만들어서 salt를 만든다 -> 이를 이용해 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err);//err뜨면 err를 index.js에 보낸다.
+        bcrypt.hash(user.password, salt, function(err, hash) {//암호화 부분
+            if(err) return next(err);//err뜨면 err를 index.js에 보낸다.
+            // Store hash in your password DB.
+            user.password=hash;//hash를 만드는데 성공하면 user.password를 hash된 비밀번호로 바꿔준다.
+            next();//index.js save로 넘어가는 명령어
+            });
+        });
+    }
+    
+    
 
 })
 
