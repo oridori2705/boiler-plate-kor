eb71fbd31e3f87d0090f896ebd912b70019a97b2
신준혁 <oridori2705@naver.com>
2023-02-19 23:35:45 +0900

    Login Route 추가 Cookie-parser 설치
    Login Route 추가 Cookie-parser 설치
    로그인에 성공하면 쿠키에 토큰 저장

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose');
 
 const bcrypt =require("bcrypt");//비밀번호 암호화 설정 : bcrypt를 가져온다.
-const saltRounds=10;
+const saltRounds=10; //salt를 위한 saltRounds 생성
+
+const jwt = require("jsonwebtoken")//다운받은 jsonwebtoken 가져옴
+
+
 
 //스키마 생성
 const userSchema = mongoose.Schema({
@@ -53,11 +57,45 @@ userSchema.pre("save",function(next){
             next();//index.js save로 넘어가는 명령어
             });
         });
+    }else{ //비밀번호만 변경하지 않은경우도 만들어줘야한다.
+        next()
     }
     
     
 
 })
+
+//loginRoute에서 같은 이메일을 찾은 DB의 정보와 비밀번호가 같은지 확인하는 부분
+//comparePassword는 우리가 임의로 만든 함수 이름이므로 바꿔도됨 그대신 index.js에서 받는 명령어도 바꿔줘야함
+//plainPassword는 맞는지 확인하는 비밀번호
+userSchema.methods.comparePassword=function(plainPassword,cb){
+    //plainPassword 1234567이고, 암호화된 비밀번호 : ~~~~~ 일 때 같은지 확인해야함
+    //그러면 plainPassword를 암호화해서 db에 있는 암호화된 비밀번호와 같은지 확인해야한다.(복호화 안됨)
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){ //가져온 비밀번호와 DB의 비밀번호가 같은지 확인
+        if(err) return cb(err);//cb는 콜백
+        cb(null,isMatch);//비밀번호가 같으면 isMatch : true를 보냄
+    })
+}
+
+
+//로그인을 성공하면 토큰을 생성하는 부분
+userSchema.methods.generateToken=function(cb){
+    //jsonwebtoken을 이용해서 token을 생성하기
+    var user =this;
+    //몽고DB에있는 _id를 가져온다
+    jwt.sign(user._id.toHexString(),'secretToken');//이 부분이 에러가 나옴->plain object가 나와야함 toHexstring으로 해결
+    //user._id+"secretToken"=token; => 나중에 해석할때 secretToken을 넣으면 -> user._id가 나온다.(누구인지 알 수있음) secretToken은 기억해야함
+
+    user.token=this.token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+
+        cb(null,user); //토큰이 저장 잘 됐으면 그 user 정보를 전달한다.
+    })
+}
+
+
+
 
 //스키마를 모델로 감싸줘야한다.
 const User = mongoose.model('User',userSchema);
